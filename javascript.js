82fdea7f61ce27ad397d2c8e07d9eb645644de45
Tazwar <tazwar9t6@gmail.com>
2020-07-23 16:07:52 +0600

    1% done lol

---
+++ javascript.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("fa-plus").addEventListener("click", function () {
+    const productNumber = document.getElementById("prodyctNumber").nodeValue;
+    const productTotalNumber = parseFloat(productNumber);
+})
+")
