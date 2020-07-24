8c9a982a85e914e01383b4c8f0e52e07d123a4ee
Tazwar <tazwar9t6@gmail.com>
2020-07-24 12:30:36 +0600

    cart remove added for test

--- javascript.js
+++
@@ -1,5 +0,0 @@
-document.getElementsByClassName("fa-plus").addEventListener("click", function () {
-    const productNumber = document.getElementById("prodyctNumber").nodeValue;
-    const productTotalNumber = parseFloat(productNumber);
-})
-")
