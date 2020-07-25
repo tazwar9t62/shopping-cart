48266fac86c6b589a04c87a595b86e1eb0eba102
Tazwar <tazwar9t6@gmail.com>
2020-07-25 17:03:52 +0600

    Done!

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
  //product 1
  //increasing items number
-
  document.getElementById("product1Plus").addEventListener("click", function () {
      quantityHandle("product1Number", "product1Price", 1, 1199);
-     //removing product1 price from subtotal , tax and total
+     //updating price at subtotal , tax and total
      updateValue();
  })
  //decreasing product1 quantity 
  document.getElementById("product1Minus").addEventListener("click", function () {
      quantityHandle("product1Number", "product1Price", -1, 1199);
-     //removing product1 price from subtotal , tax and total
+     //updating price at subtotal , tax and total
      updateValue();
  })
-
  //removing cart 1
  document.getElementById("product1Remove").addEventListener("click", function () {
      removeCart("product1Number", "product1Cart");
-
-
  })
 
  //Product 2
  //increasing product2 quantity 
  document.getElementById("product2Plus").addEventListener("click", function () {
      quantityHandle("product2Number", "product2Price", 1, 10);
-     //adding product 2 price in subtotal,tax and total
+     //updating price at subtotal , tax and total
      updateValue();
-
  })
-
  //decreasing product2 quantity 
  document.getElementById("product2Minus").addEventListener("click", function () {
      quantityHandle("product2Number", "product2Price", -1, 10);
-
-     //removing product2 price from subtotal , tax and total
+     //updating price at subtotal , tax and total
      updateValue();
-
  })
-
  //removing cart 2
  document.getElementById("product2Remove").addEventListener("click", function () {
      removeCart("product2Number", "product2Cart");
-
  })
 
  //Functions
@@ -53,12 +43,8 @@
      const inputValueFloat2 = parseInt(inputValue2);
      const total1 = (inputValueFloat1 * 1199) + (inputValueFloat2 * 10);
      document.getElementById("subtotalAmount").innerText = total1.toFixed(2);
-
-
      const total2 = (5 / 100) * total1;
      document.getElementById("taxAmount").innerText = total2.toFixed(2);
-
-
      const total3 = total1 + total2;
      document.getElementById("totalAmount").innerText = total3.toFixed(2);
  }
@@ -67,14 +53,12 @@
  function quantityHandle(id, id2, IncreaseOrDecrease, productPrice) {
      const number = document.getElementById(id).value;
      const numberFloat = parseFloat(number);
-
      //Increasing or decreasing the product  quantity
      const totalNumber = numberFloat + IncreaseOrDecrease;
      document.getElementById(id).value = totalNumber;
      if (totalNumber < 0) {
          return document.getElementById(id).value = 0;
      }
-
      //increasing or decreasing product price
      const priceTotal = totalNumber * productPrice;
      document.getElementById(id2).innerText = priceTotal;
@@ -83,6 +67,6 @@
  //function to remove cart
  function removeCart(id, idRemove) {
      document.getElementById(id).value = 0;
-     updateValue();
-     document.getElementById(idRemove).style.display = "none";
+     updateValue(); //updating price at subtotal , tax and total
+     document.getElementById(idRemove).style.display = "none"; //Removing the cart
  }
