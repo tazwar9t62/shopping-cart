5effa8022d103fae837b908782af05f63930bfa5
Tazwar <tazwar9t6@gmail.com>
2020-07-25 16:58:33 +0600

    Finally 100% done!

--- script.js
+++ script.js
@@ -4,22 +4,20 @@
  document.getElementById("product1Plus").addEventListener("click", function () {
      quantityHandle("product1Number", "product1Price", 1, 1199);
      //removing product1 price from subtotal , tax and total
-     updateValue("subtotalAmount", "taxAmount", "totalAmount", 1199);
+     updateValue();
  })
  //decreasing product1 quantity 
  document.getElementById("product1Minus").addEventListener("click", function () {
      quantityHandle("product1Number", "product1Price", -1, 1199);
      //removing product1 price from subtotal , tax and total
-     updateValue("subtotalAmount", "taxAmount", "totalAmount", -1199);
+     updateValue();
  })
 
  //removing cart 1
  document.getElementById("product1Remove").addEventListener("click", function () {
-     document.getElementById("product1Cart").style.display = "none";
-     const numberTest = document.getElementById("product1Number").value;
-     const numberFloatTest = parseFloat(numberTest);
-     //removing total price for product1 in subtotal , tax and total
-     updateValue("subtotalAmount", "taxAmount", "totalAmount", numberFloatTest * -1199);
+     removeCart("product1Number", "product1Cart");
+
+
  })
 
  //Product 2
@@ -27,7 +25,8 @@
  document.getElementById("product2Plus").addEventListener("click", function () {
      quantityHandle("product2Number", "product2Price", 1, 10);
      //adding product 2 price in subtotal,tax and total
-     updateValue("subtotalAmount", "taxAmount", "totalAmount", 10);
+     updateValue();
+
  })
 
  //decreasing product2 quantity 
@@ -35,35 +34,33 @@
      quantityHandle("product2Number", "product2Price", -1, 10);
 
      //removing product2 price from subtotal , tax and total
-     updateValue("subtotalAmount", "taxAmount", "totalAmount", -10);
+     updateValue();
+
  })
 
  //removing cart 2
  document.getElementById("product2Remove").addEventListener("click", function () {
-     document.getElementById("product2Cart").style.display = "none";
-     const numberTest2 = document.getElementById("product2Number").value;
-     const numberFloatTest2 = parseFloat(numberTest2);
-     //removing total price for product2 in subtotal , tax and total
-     updateValue("subtotalAmount", "taxAmount", "totalAmount", numberFloatTest2 * -10);
+     removeCart("product2Number", "product2Cart");
+
  })
 
  //Functions
  //Function for updating values
- function updateValue(subTotal, tax, total, value) {
-     const inputValue1 = document.getElementById(subTotal).innerText;
-     const inputValueFloat1 = parseFloat(inputValue1);
-     const total1 = inputValueFloat1 + value;
-     document.getElementById(subTotal).innerText = total1.toFixed(2);
+ function updateValue(tax, total) {
+     const inputValue1 = document.getElementById("product1Number").value;
+     const inputValueFloat1 = parseInt(inputValue1);
+     const inputValue2 = document.getElementById("product2Number").value;
+     const inputValueFloat2 = parseInt(inputValue2);
+     const total1 = (inputValueFloat1 * 1199) + (inputValueFloat2 * 10);
+     document.getElementById("subtotalAmount").innerText = total1.toFixed(2);
 
-     const inputValue2 = document.getElementById(tax).innerText;
-     const inputValueFloat2 = parseFloat(inputValue2);
-     const total2 = ((5 / 100) * value) + inputValueFloat2;
-     document.getElementById(tax).innerText = total2.toFixed(2);
 
-     const inputValue3 = document.getElementById(total).innerText;
-     const inputValueFloat3 = parseFloat(inputValue3);
-     const total3 = ((5 / 100) * value) + value + inputValueFloat3;
-     document.getElementById(total).innerText = total3.toFixed(2);
+     const total2 = (5 / 100) * total1;
+     document.getElementById("taxAmount").innerText = total2.toFixed(2);
+
+
+     const total3 = total1 + total2;
+     document.getElementById("totalAmount").innerText = total3.toFixed(2);
  }
 
  //Function for quantity handle
@@ -81,4 +78,11 @@
      //increasing or decreasing product price
      const priceTotal = totalNumber * productPrice;
      document.getElementById(id2).innerText = priceTotal;
+ }
+
+ //function to remove cart
+ function removeCart(id, idRemove) {
+     document.getElementById(id).value = 0;
+     updateValue();
+     document.getElementById(idRemove).style.display = "none";
  }
